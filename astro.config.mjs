d18f713261104dbda29ca511c6338555f8569e1c
Ben Keys <bkeys818@gmail.com>
2023-03-04 19:30:55 -0500

    🎨 Format code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    config: { applyBaseStyles: false }
-  })]
-});
+  integrations: [
+    tailwind({
+      config: { applyBaseStyles: false },
+    }),
+  ],
+})
